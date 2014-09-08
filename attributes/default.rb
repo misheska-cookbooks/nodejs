d846731d1d0abe09e2a1a17fb73bdaddb33c3d02
Mischa Taylor <mischa@misheska.com>
2014-09-07 22:27:13 -0700

    Adding support for latest string as a version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,13 @@
 default['nodejs']['install_dir'] = '/usr/local'
-default['nodejs']['version'] = '0.10.31'
-default['nodejs']['binary_url'] = "http://nodejs.org/dist" \
-  "/v#{node['nodejs']['version']}" \
-  "/node-v#{node['nodejs']['version']}-linux-x64.tar.gz"
-default['nodejs']['source_url'] = "http://nodejs.org/dist" \
-  "/v#{node['nodejs']['version']}/node-v#{node['nodejs']['version']}.tar.gz"
+default['nodejs']['version'] = 'latest'
+nodejs_version = node['nodejs']['version']
+if nodejs_version.eql?('latest')
+  default['nodejs']['binary_url'] = "http://nodejs.org/dist" \
+    "/latest/node-v0.10.31-linux-x64.tar.gz"
+  default['nodejs']['source_url'] = 'http://nodejs.org/dist/node-latest.tar.gz'
+else
+  default['nodejs']['binary_url'] = "http://nodejs.org/dist" \
+    "/v#{nodejs_version}/node-v#{nodejs_version}-linux-x64.tar.gz"
+  default['nodejs']['source_url'] = "http://nodejs.org/dist" \
+    "/v#{nodejs_version}/node-v#{nodejs_version}.tar.gz"
+end
--- recipes/source.rb
+++ recipes/source.rb
@@ -5,7 +5,7 @@ nodejs_source_url = node['nodejs']['source_url']
 nodejs_tar_gz = ::File.basename(nodejs_source_url)
 nodejs_basename = ::File.basename(nodejs_source_url, '.tar.gz')
 nodejs_tarball_path = "#{Chef::Config[:file_cache_path]}/#{nodejs_tar_gz}"
-nodejs_extract_path = Chef::Config[:file_cache_path]
+nodejs_extract_path = "#{Chef::Config[:file_cache_path]}/#{nodejs_basename}"
 nodejs_install_path = node['nodejs']['install_dir']
 
 # node_version = Mixlib::ShellOut.new("#{nodejs_extract_path}/bin/node --version")
@@ -18,21 +18,25 @@ num_cores = Mixlib::ShellOut.new('cat /proc/cpuinfo | grep processor | wc -l')
 num_cores.run_command
 num_build_threads = num_cores.stdout.to_i + 1
 
+directory nodejs_extract_path
+
 remote_file nodejs_tarball_path do
+  Chef::Log::info("Downloading #{nodejs_tarball_path} from #{nodejs_source_url}")
   source nodejs_source_url
   notifies :run, "execute[unpack #{nodejs_tarball_path}]", :immediately
   notifies :run, "execute[build #{nodejs_tarball_path}]", :immediately
 end
 
 execute "unpack #{nodejs_tarball_path}" do
-  command "tar xvf #{nodejs_tarball_path} -C #{nodejs_extract_path}"
+  Chef::Log::info("Extracting #{nodejs_tarball_path} to #{nodejs_extract_path}")
+  command "tar --strip-components=1 -xvf #{nodejs_tarball_path} -C #{nodejs_extract_path}"
   action :nothing
   only_if { install_needed }
 end
 
 execute "build #{nodejs_tarball_path}" do
   command "./configure --prefix=#{nodejs_install_path} && make -j #{num_build_threads.to_s} && make install"
-  cwd "#{nodejs_extract_path}/#{nodejs_basename}"
+  cwd "#{nodejs_extract_path}"
   action :nothing
   only_if { install_needed }
 end
